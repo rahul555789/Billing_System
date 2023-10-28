49007e586448e0bda0921d93f4591ee2a3e5134b
Kalpesh Lakum <lakumkalpesh305@gmail.com>
2023-10-28 23:16:29 +0530

    Basic interface and making total

--- header.hpp
+++ header.hpp
@@ -1,4 +1,5 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdint.h>
 
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ class Menu
 {
     public:
 
-    Menu(int num , std::string it , int pri)
+    Menu(uint16_t num , std::string it , uint16_t pri)
     {
         number=num;
         item=it;
@@ -17,29 +17,66 @@ class Menu
 
     }
 
-    void display()
-    {
+    void display(){
         std::cout << number << "\t:\t" << item << "\t\t:\t" << price << std::endl;
     }
-    int number;
+    uint16_t number;
     std::string item;
-    int price;
+    uint16_t price;
 };
 
 
 
 int main()
 {
+    
     std::vector<Menu> Vector_Menu;
 
-    Vector_Menu.push_back(Menu(1,"soup",100));
-    Vector_Menu.push_back(Menu(2,"roti",10));
-    Vector_Menu.push_back(Menu(3,"sabji",200));
-    Vector_Menu.push_back(Menu(4,"Chaas",50));
-    Vector_Menu.push_back(Menu(5,"papad",20));
+    Vector_Menu.push_back(Menu(0,"soup",100));
+    Vector_Menu.push_back(Menu(1,"roti",10));
+    Vector_Menu.push_back(Menu(2,"sabji",200));
+    Vector_Menu.push_back(Menu(3,"Chaas",50));
+    Vector_Menu.push_back(Menu(4,"papad",20));
+    Vector_Menu.push_back(Menu(5,"naan",35));
+    Vector_Menu.push_back(Menu(6,"machurian",150));
+    Vector_Menu.push_back(Menu(7,"rice",75));
+    Vector_Menu.push_back(Menu(8,"dal",55));
+    Vector_Menu.push_back(Menu(9,"chai",15));
 
-    for(Menu obj : Vector_Menu)
-    {
+    for(Menu obj : Vector_Menu) {
         obj.display();
     }
+
+    //TODO add option for various task (add_item, remove_item, edit_menu)
+    std::cout << "enter 255 to make total"  << std::endl;
+    std::cout << "enter index no : ";
+
+    std::vector<Menu> cart;
+    uint16_t index_no = -1;
+
+    while(index_no != 255) {
+        
+        //TODO take input of quentity of same item and add it to cart
+        std::cin >> index_no;
+        
+        if(index_no > 9 || index_no < 0 ) {
+            if(index_no != 255)
+                std::cout << "there is no item with " << index_no << std:: endl;
+        }
+
+        else {
+            //TODO clear screen before every cart print
+            cart.push_back(Vector_Menu[index_no]);
+            std::cout << "items on cart " << std::endl;
+
+            for(Menu obj : cart) {
+                obj.display();
+            }
+        }
+    }
+    uint32_t total = 0;
+    for(Menu obj : cart) {
+        total += obj.price;
+    }
+    std::cout << "your total is = " << total << std::endl;
 }
